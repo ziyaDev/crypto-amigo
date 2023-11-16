34baae51dd25fd49f6458b07fed10dca3c73d938
Chi Nguyen <chinguyen4646@gmail.com>
2023-11-16 14:52:31 +0000

    Set up client and server for app

--- www/vite.config.js.timestamp-1699291890721-b5ff199394dd8.mjs
+++ www/vite.config.js.timestamp-1699291890721-b5ff199394dd8.mjs
@@ -1,10 +1,8 @@
 // vite.config.js
-import { defineConfig } from "file:///Users/chinguyen/Desktop/crypto-amigo/client/node_modules/vite/dist/node/index.js";
-import react from "file:///Users/chinguyen/Desktop/crypto-amigo/client/node_modules/@vitejs/plugin-react-swc/index.mjs";
+import { defineConfig } from 'file:///Users/chinguyen/Desktop/crypto-amigo/www/node_modules/vite/dist/node/index.js';
+import react from 'file:///Users/chinguyen/Desktop/crypto-amigo/www/node_modules/@vitejs/plugin-react-swc/index.mjs';
 var vite_config_default = defineConfig({
-  plugins: [react()]
+  plugins: [react()],
 });
-export {
-  vite_config_default as default
-};
+export { vite_config_default as default };
 //# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcuanMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImNvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9kaXJuYW1lID0gXCIvVXNlcnMvY2hpbmd1eWVuL0Rlc2t0b3AvY3J5cHRvLWFtaWdvL2NsaWVudFwiO2NvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9maWxlbmFtZSA9IFwiL1VzZXJzL2NoaW5ndXllbi9EZXNrdG9wL2NyeXB0by1hbWlnby9jbGllbnQvdml0ZS5jb25maWcuanNcIjtjb25zdCBfX3ZpdGVfaW5qZWN0ZWRfb3JpZ2luYWxfaW1wb3J0X21ldGFfdXJsID0gXCJmaWxlOi8vL1VzZXJzL2NoaW5ndXllbi9EZXNrdG9wL2NyeXB0by1hbWlnby9jbGllbnQvdml0ZS5jb25maWcuanNcIjtpbXBvcnQgeyBkZWZpbmVDb25maWcgfSBmcm9tICd2aXRlJ1xuaW1wb3J0IHJlYWN0IGZyb20gJ0B2aXRlanMvcGx1Z2luLXJlYWN0LXN3YydcblxuLy8gaHR0cHM6Ly92aXRlanMuZGV2L2NvbmZpZy9cbmV4cG9ydCBkZWZhdWx0IGRlZmluZUNvbmZpZyh7XG4gIHBsdWdpbnM6IFtyZWFjdCgpXSxcbn0pXG4iXSwKICAibWFwcGluZ3MiOiAiO0FBQXNULFNBQVMsb0JBQW9CO0FBQ25WLE9BQU8sV0FBVztBQUdsQixJQUFPLHNCQUFRLGFBQWE7QUFBQSxFQUMxQixTQUFTLENBQUMsTUFBTSxDQUFDO0FBQ25CLENBQUM7IiwKICAibmFtZXMiOiBbXQp9Cg==
