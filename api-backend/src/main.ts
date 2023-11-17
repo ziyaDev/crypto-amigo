c0ac3da46c1d61e6fbab4b57962cb482f4955f05
Chi Nguyen <chinguyen4646@gmail.com>
2023-11-17 11:06:22 +0000

    change /server to /api-backend

--- api-backend/src/main.ts
+++ api-backend/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
-  console.log("Server running now on port 8080...");
+  console.log("api-backend server now running on port 8080...");
 }
 bootstrap();
