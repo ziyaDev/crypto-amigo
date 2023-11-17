79150e8c5c326747e52961c9f6f03cfa4db016b5
Chi Nguyen <chinguyen4646@gmail.com>
2023-11-17 20:28:47 +0000

    Not needed

--- api-backend/src/app.module.ts
+++ api-backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserModule } from "./user/user.module";
 import { ConfigModule, ConfigService } from "@nestjs/config";
@@ -25,7 +23,5 @@ import { User } from "./user/entities/user.entity";
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
