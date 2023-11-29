12317219c05bdc92bf6e9f47740363e5c7a5748e
Chi Nguyen <chinguyen4646@gmail.com>
2023-11-29 23:16:21 +0000

    custom errors on accesstoken guard

--- api-backend/src/common/guards/accessToken.guard.ts
+++ api-backend/src/common/guards/accessToken.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { TokenExpiredError, JsonWebTokenError } from "jsonwebtoken";
 
 @Injectable()
-export class AccessTokenGuard extends AuthGuard("jwt") {}
+export class AccessTokenGuard extends AuthGuard("jwt") {
+  handleRequest(err: any, user: any, info: any, context: any, status: any) {
+    if (info instanceof TokenExpiredError) {
+      throw new UnauthorizedException("Token expired!");
+    } else if (info instanceof JsonWebTokenError) {
+      throw new UnauthorizedException("Invalid Token!");
+    }
+
+    return super.handleRequest(err, user, info, context, status);
+  }
+}
